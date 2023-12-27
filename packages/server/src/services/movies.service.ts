3b8e83a82d0f5aeceb66ab90661ced70e1cc4ccf
Nicolas Montiel <nicolasmontielf@gmail.com>
2023-12-27 00:01:53 -0300

    Initial implementation of TMDB

---
+++ packages/server/src/services/movies.service.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+function getInstance() {
+    return axios.create({
+        baseURL: 'https://api.themoviedb.org/3',
+        timeout: 5000,
+        headers: {
+            'Authorization': `Bearer ${process.env.TMDB_API_KEY}`
+        }
+    })
+}
+ 
+export async function getGenres() {
+    const instance = getInstance()
+    return await instance.get('/genre/movie/list')
+}
